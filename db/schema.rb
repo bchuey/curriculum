a970afad481c6baed15bc87bd74c3f028a878348
steven <slstevenlam@gmail.com>
2015-10-24 22:25:44 -0700

    pushing steven branch before merging with chris

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024091512) do
+ActiveRecord::Schema.define(version: 20151024122156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,12 +102,10 @@ ActiveRecord::Schema.define(version: 20151024091512) do
     t.string   "last_name"
     t.string   "password_digest"
     t.string   "email"
-    t.integer  "role_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
-
-  add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
 
   create_table "votes", force: true do |t|
     t.integer  "post_id"
